cc7b00387aaab7a9941cc1d47c92498f310739f9
Eric Chai <electromatter@gmail.com>
2017-07-19 16:01:32 -0400

    added spin computer

---
+++ spin.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+def displacement(perm):
+    perm = list(perm)
+    if set(perm) != set(range(len(perm))):
+        raise ValueError('must be a permutation of [0, 1, 2, ..., n-1]')
+    return [p_i - i for i, p_i in enumerate(perm)]
+
+def spin(perm):
+    disp = displacement(perm)
+    while True:
+        maximum, minimum = max(disp), min(disp)
+        if maximum - minimum <= len(disp):
+            break
+        xi, ni = disp.index(maximum), disp.index(minimum)
+        disp[xi] -= len(disp)
+        disp[ni] += len(disp)
+    return disp
+
+def main():
+    while True:
+        try:
+            perm = eval(input())
+            print(perm, '=>', spin(perm))
+        except ValueError:
+            print()
+        except EOFError:
+            break
+
+if __name__ == '__main__':
+    main()
